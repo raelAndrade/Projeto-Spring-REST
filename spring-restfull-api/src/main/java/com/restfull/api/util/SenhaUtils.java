233d2b756c2f371811a002642b38991c6c96b1a8
raelAndrade <israelg.andrade@bol.com.br>
2017-12-11 14:12:57 -0200

    criação do arquivo para gerar tabelas atraves do flyway

--- spring-restfull-api/src/main/java/com/restfull/api/util/SenhaUtils.java
+++ spring-restfull-api/src/main/java/com/restfull/api/util/SenhaUtils.java
@@ -1,5 +1,19 @@
 package com.restfull.api.util;
 
-public class SenhaUtils {
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+public class SenhaUtils {
+	public static String gerarBCrypt(String senha) {
+		if(senha == null) {
+			return senha;
+		}
+		
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		return bCryptEncoder.encode(senha);
+	}
+	
+	public static boolean senhaValida(String senha, String senhaEncoded) {
+		BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+		return bCryptPasswordEncoder.matches(senha, senhaEncoded);
+	}
 }
